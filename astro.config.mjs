173f40a7ef1a6da8bcc934caedc80550f651dab9
loq <ALE@LAPTOP-UUM7DVB6>
2026-08-06 17:51:54 +0700

    fix: enable cloudflare pages functions routing for astro ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough',
+    routes: {
+      strategy: 'include',
+      include: ['/*'],
+      exclude: ['/_astro/*'],
+    },
     platformProxy: {
       enabled: true,
     },
