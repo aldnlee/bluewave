3dfa4197116359bdcab94d52ee8164c3b89b17bb
loq <ALE@LAPTOP-UUM7DVB6>
2026-08-06 18:05:55 +0700

    fix: remove wrangler.json and disable session kv binding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', 
-  
+  output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough',
+    session: false, // Matikan KV session auto-binding
   }),
-
 });
